c906b640f15c56ab67ac5684b1048ae86b4e9411
shabaka computers <aadilvazhakkad@gmail.com>
2023-06-03 22:22:27 +0530

    boolean issues3

--- src/main/java/com/driver/model/User.java
+++ src/main/java/com/driver/model/User.java
@@ -25,7 +25,7 @@ public class User {
     private String password;
     private String originalIp;
     private String maskedIp;
-    private boolean connected;
+    private Boolean connected;
 
     @OneToOne
     @JoinColumn

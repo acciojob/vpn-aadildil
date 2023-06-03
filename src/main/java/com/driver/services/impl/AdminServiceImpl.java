6b6bb4fbfe75c9d4cb1abac6792cbec438e362e7
shabaka computers <aadilvazhakkad@gmail.com>
2023-06-03 22:51:19 +0530

    boolean issues7

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -56,58 +56,96 @@ public class AdminServiceImpl implements AdminService {
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
 
-        Optional<ServiceProvider> optionalServiceProvider=serviceProviderRepository1.findById(serviceProviderId);
-        ServiceProvider serviceProvider=optionalServiceProvider.get();
+//        Optional<ServiceProvider> optionalServiceProvider=serviceProviderRepository1.findById(serviceProviderId);
+//        ServiceProvider serviceProvider=optionalServiceProvider.get();
+//
+//
+//
+//
+//        if(isCountryPresent(countryName)==false)
+//            throw new Exception("Country not found");
+//        Country country=createCountry(countryName);
+//        country.setServiceProvider(serviceProvider);
+//        serviceProvider.getCountryList().add(country);
+//        serviceProviderRepository1.save(serviceProvider);
+//
+//        return serviceProvider;
 
+        if(countryName.equalsIgnoreCase("IND") || countryName.equalsIgnoreCase("USA") || countryName.equalsIgnoreCase("JPN") || countryName.equalsIgnoreCase("CHI") || countryName.equalsIgnoreCase("AUS")){
+            Country country = new Country();
 
+            ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
 
+            if(countryName.equalsIgnoreCase("IND")){
+                country.setCountryName(CountryName.IND);
+                country.setCode(CountryName.IND.toCode());
+            }
+            if(countryName.equalsIgnoreCase("USA")){
+                country.setCountryName(CountryName.USA);
+                country.setCode(CountryName.USA.toCode());
+            }
+            if(countryName.equalsIgnoreCase("JPN")){
+                country.setCountryName(CountryName.JPN);
+                country.setCode(CountryName.JPN.toCode());
+            }
+            if(countryName.equalsIgnoreCase("CHI")){
+                country.setCountryName(CountryName.CHI);
+                country.setCode(CountryName.CHI.toCode());
+            }
+            if(countryName.equalsIgnoreCase("AUS")){
+                country.setCountryName(CountryName.AUS);
+                country.setCode(CountryName.AUS.toCode());
+            }
+            country.setServiceProvider(serviceProvider);
+            serviceProvider.getCountryList().add(country);
+            serviceProviderRepository1.save(serviceProvider);
 
-        if(isCountryPresent(countryName)==false)
+            return serviceProvider;
+        }
+        else{
             throw new Exception("Country not found");
-        Country country=createCountry(countryName);
-        country.setServiceProvider(serviceProvider);
-        serviceProvider.getCountryList().add(country);
-        serviceProviderRepository1.save(serviceProvider);
+        }
 
-        return serviceProvider;
 
 
 
 
     }
 
-    public Boolean isCountryPresent(String countryName) {////////////////
-
-        if(countryName.equalsIgnoreCase("IND") || countryName.equalsIgnoreCase("USA") || countryName.equalsIgnoreCase("JPN") || countryName.equalsIgnoreCase("CHI") || countryName.equalsIgnoreCase("AUS"))
-           return true;
-      return false;
-    }
-
-    public Country createCountry(String countryName)
-    {
-        Country country=new Country();
-
-        if(countryName.equalsIgnoreCase("IND")){
-            country.setCountryName(CountryName.IND);
-            country.setCode(CountryName.IND.toCode());
-        }
-        if(countryName.equalsIgnoreCase("USA")){
-            country.setCountryName(CountryName.USA);
-            country.setCode(CountryName.USA.toCode());
-        }
-        if(countryName.equalsIgnoreCase("JPN")){
-            country.setCountryName(CountryName.JPN);
-            country.setCode(CountryName.JPN.toCode());
-        }
-        if(countryName.equalsIgnoreCase("CHI")){
-            country.setCountryName(CountryName.CHI);
-            country.setCode(CountryName.CHI.toCode());
-        }
-        if(countryName.equalsIgnoreCase("AUS")){
-            country.setCountryName(CountryName.AUS);
-            country.setCode(CountryName.AUS.toCode());
-        }
-        return country;
-    }
+//    public Boolean isCountryPresent(String countryName) {////////////////
+//
+//        if(countryName.equalsIgnoreCase("IND") || countryName.equalsIgnoreCase("USA") || countryName.equalsIgnoreCase("JPN") || countryName.equalsIgnoreCase("CHI") || countryName.equalsIgnoreCase("AUS"))
+//           return true;
+//      return false;
+//    }
+//
+//    public Country createCountry(String countryName)
+//    {
+//        Country country=new Country();
+//
+//        if(countryName.equalsIgnoreCase("IND")){
+//            country.setCountryName(CountryName.IND);
+//            country.setCode(CountryName.IND.toCode());
+//        }
+//        if(countryName.equalsIgnoreCase("USA")){
+//            country.setCountryName(CountryName.USA);
+//            country.setCode(CountryName.USA.toCode());
+//        }
+//        if(countryName.equalsIgnoreCase("JPN")){
+//            country.setCountryName(CountryName.JPN);
+//            country.setCode(CountryName.JPN.toCode());
+//        }
+//        if(countryName.equalsIgnoreCase("CHI")){
+//            country.setCountryName(CountryName.CHI);
+//            country.setCode(CountryName.CHI.toCode());
+//        }
+//        if(countryName.equalsIgnoreCase("AUS")){
+//            country.setCountryName(CountryName.AUS);
+//            country.setCode(CountryName.AUS.toCode());
+//        }
+//        return country;
+//
+//
+//    }
 
 }
